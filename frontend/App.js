a7009f6ebf3cd536c760529205c61d3725343078
Kevin Liao <me@liaokev.in>
2021-04-28 17:27:15 +0800

    Whoops.

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,8 @@
 // @flow
-import React, { useState, useEffect } from 'react';
-import * as Papa from 'papaparse';
+import React from 'react';
 import Map from './Map';
 
 export default function App(): React.Component {
-    const [data, setData] = useState({ hospitals: [] });
-
-    useEffect(async () => {
-        const response = await fetch('./hospitals.csv');
-        const text = await response.text();
-        console.log("Response text: ", text);
-        const result = Papa.parse(text, { header: true });
-        setData(result.data);
-        console.log(result.data);
-    });
-
     return <div>
         <Map />
     </div>;
