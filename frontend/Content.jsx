df0d13e8264b54240e50d329b4119dbcdc0cfbe5
Kevin Liao <me@liaokev.in>
2021-05-04 12:10:38 +0800

    Add Bahasa translations
    Thanks to Dakota Pekerti for these!

--- frontend/Content.jsx
+++ frontend/Content.jsx
@@ -64,8 +64,30 @@ Please help me translate this text. Contribute by adding to
 `;
 
 const bahasa = `
-Bahasa translation is a work in progress. Please contribute by 
-adding to [this file](https://github.com/g0v/vaccinate/blob/master/frontend/Content.jsx)
+### Siapa yang bisa mendapatkan vaksin COVID-19?
+Anda bisa mendapatkan vaksin COVID-19 secara gratis jika Anda termasuk dalam
+kelompok yang memenuhi syarat yang ditetapkan oleh pemerintah Taiwan. Jika Anda
+berniat bepergian ke luar Taiwan, Anda bisa membayar sendiri untuk mendapatkan vaksin.
+
+### Vaksin apa yang tersedia?
+Saat ini, pemerintah Taiwan sedang mengelola Vaksin AstraZeneca.
+
+### Bagaimana cara menggunakan situs web ini?
+Anda bisa menggunakannya untuk mencari rumah sakit di daerah Anda. Jika
+kolom 'Ketersediaan' di sebelah kanan bertuliskan 'Tersedia', maka akan ada
+vaksin COVID-19 yang bisa diberikan. Jika tertulis 'Tidak Tersedia', maka
+rumah sakit tersebut tidak ada janji terbuka. Jika tertulis 'Tidak Ada Data',
+artinya kami belum menemukan cara untuk menambahkan data ke situs web ini.
+Tolong cek rumah sakit langsung dengan mengklik 'Daftar di sini'.
+
+### Bagaimana saya dapat mempelajari lebih lanjut? *
+Ada bisa melihat [FAQ Bahasa Inggris oleh CDC Taiwan](https://www.cdc.gov.tw/File/Get/rJJ09nktKU7btX_ZTEo_4w)
+
+### Siapa yang membangun situs web ini?
+Situs web ini dibuat oleh sukarelawan dengan jaringan peretas sipil
+[g0v](https://g0v.tw). Kami tidak terkait dengan pemerintah Taiwan
+dan bukan merupakan sumber resmi. Untuk informasi resmi, silakan cek
+[situs web CDC Taiwan] (https://cdc.gov.tw). `
 `;
 
 const ja = `
