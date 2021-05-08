6e997065c732dfaffeb5307366ceabdc17a041cf
Kevin Liao <me@liaokev.in>
2021-05-09 05:14:25 +0800

    Hide map unless on Medium-sized screen.

--- frontend/Map.jsx
+++ frontend/Map.jsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 
 export default function Map(): React.Node {
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div className='d-none d-md-block' style={{ textAlign: 'center' }}>
       <iframe
         src="https://www.google.com/maps/d/embed?mid=1LUqWZfmEBZFCAl8ThcE1cakLsb9k-Pkr"
         title="Map"
