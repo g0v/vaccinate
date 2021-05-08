4a23ed8aa041a6f2ab263cd45282aee817063ad1
Kevin Liao <me@liaokev.in>
2021-05-09 05:07:45 +0800

    Resize map iframe

--- frontend/Map.jsx
+++ frontend/Map.jsx
@@ -3,12 +3,11 @@ import * as React from 'react';
 
 export default function Map(): React.Node {
   return (
-    <div className="container" style={{ textAlign: 'center' }}>
+    <div style={{ textAlign: 'center' }}>
       <iframe
         src="https://www.google.com/maps/d/embed?mid=1LUqWZfmEBZFCAl8ThcE1cakLsb9k-Pkr"
-        width="640"
-        height="480"
         title="Map"
+        style={{width: '100%', height: 500}}
       />
     </div>
   );
