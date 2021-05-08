2facb2b79153795b797f640d17cc847b24897a32
Kevin Liao <me@liaokev.in>
2021-05-09 01:16:19 +0800

    Make nested array and feed data into cards.

--- frontend/Card.jsx
+++ frontend/Card.jsx
@@ -1,11 +1,22 @@
 // @flow
 import * as React from 'react';
 
-export default function Card(): React.Node {
+export default function Card(props: {
+  name: string,
+  location: string,
+  department: string,
+  phone: string,
+  address: string,
+  website: string,
+  availability: string,
+}): React.Node {
+  const {
+    name, location, department, phone, address, website, availability,
+  } = props;
   return (
     <div className="card">
       <div className="card-body">
-        <h5 className="card-title">Hospital Name</h5>
+        <h5 className="card-title">{name}</h5>
         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card&apos;s content.</p>
         <a href="/" className="btn btn-primary">Go somewhere</a>
         <p className="card-text">
--- frontend/Table.jsx
+++ frontend/Table.jsx
@@ -7,21 +7,31 @@ import type { Hospital } from './Row';
 
 export default function Table(props: { rows: Array<Hospital> }): React.Node {
   const { rows } = props;
+  const nestedRows = rows.reduce(
+    (accumulator, currentValue, index) =>
+      index % 4 === 0
+        ? accumulator.concat([rows.slice(index, index + 4)])
+        : accumulator
+    , []);
+  console.log(nestedRows);
   return (
     <div>
-      <div className="row row-cols-4">
-        {rows.map(() => (
-          <div className="col">
-            <Card />
-          </div>
-        ))}
-      </div>
-      <div className="row row-cols-4">
-        <div className="col" />
-        <div className="col">Column</div>
-        <div className="col">Column</div>
-        <div className="col">Column</div>
-      </div>
+      {nestedRows.map(row => (
+        <div className="row row-cols-4">
+          {row.map(col =>
+            <div className="col">
+              <Card key={col.hospitalId.toString()}
+                hospitalId={col.hospitalId}
+                name={col.name}
+                location={col.location}
+                department={col.department}
+                phone={col.phone}
+                address={col.address}
+                website={col.website}
+                availability={col.availability} />
+            </div>)}
+        </div>
+      ))}
       <table className="table">
         <thead>
           <tr>
