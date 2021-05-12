989be7e15263aaeead88b1d291d8d1f02f807b8d
Kevin Liao <me@liaokev.in>
2021-05-12 17:29:20 +0800

    Add basic frontend for supporting public vaccines.

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -7,10 +7,12 @@ import Spinner from './Components/Spinner';
 import G0vbar from './Components/G0vbar';
 
 import type { Locale } from './Types/Locale';
+import type { VaccineType } from './Types/VaccineType';
 
 export default function App(): React.Node {
   const [rows, setRows] = React.useState([]);
   const [locale: Locale, setLocale] = React.useState('zh');
+  const [vaccineType: VaccineType, setVaccineType] = React.useState('SelfPaid');
   const url = './hospitals';
   fetch(url).then((data) => data.json()).then((res) => setRows(res));
   return (
@@ -27,8 +29,31 @@ export default function App(): React.Node {
           </div>
         </div>
         <h2 style={{ textAlign: 'center' }}>Vaccination Availability</h2>
+        <div style={{ textAlign: 'center' }}>
+          <div
+            className="btn-group"
+            role="group"
+            aria-label="Basic outlined example"
+          >
+            <button
+              type="button"
+              onClick={() => setVaccineType('SelfPaid')}
+              className="btn btn-outline-primary"
+            >
+              Self-Paid Vaccine
+            </button>
+            <button
+              type="button"
+              onClick={() => setVaccineType('GovernmentPaid')}
+              className="btn btn-outline-primary"
+            >
+              Government-Paid Vaccine
+            </button>
+          </div>
+        </div>
         {rows.length === 0 ? <Spinner />
-          : <VaccineDataGrid rows={rows} locale={locale} />}
+          : <VaccineDataGrid
+            vaccineType={vaccineType} rows={rows} locale={locale} />}
         <p><i>Created with love by a member of g0v, Taiwans civic tech community.</i></p>
       </div>
     </div>
---
+++ frontend/Types/VaccineType.js
@@ -0,0 +1,3 @@
+// @flow
+
+export type VaccineType = 'SelfPaid' | 'GovernmentPaid';
