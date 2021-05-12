aabb0ac5322c33af9104baebacbe230441d8497c
Kevin Liao <me@liaokev.in>
2021-05-12 20:31:46 +0800

    Padding and styling.

--- frontend/Components/VaccineDataGrid.jsx
+++ frontend/Components/VaccineDataGrid.jsx
@@ -20,39 +20,44 @@ export default function VaccineDataGrid(
   const unavailableHospitals = rows.filter((row) => getAvailability(row) === 'Unavailable');
   const noDataHospitals = rows.filter((row) => getAvailability(row) === 'No data');
 
-  const makeCardGrid = (hospitals: Array<Hospital>, buttonText: string) => (
-    <div className="row row-cols-1 row-cols-md-4 g-3">
-      {hospitals.map((hospital) => (
-        <div className="col" key={hospital.hospitalId.toString()}>
-          <Card
-            address={hospital.address}
-            availability={getAvailability(hospital)}
-            buttonText={buttonText}
-            department={hospital.department}
-            hospitalId={hospital.hospitalId}
-            locale={locale}
-            location={hospital.location}
-            name={hospital.name}
-            phone={hospital.phone}
-            website={hospital.website}
-          />
-        </div>
-      ))}
-    </div>
-  );
+  const makeCardGrid = (hospitals: Array<Hospital>, buttonText: string) => (hospitals.length !== 0
+    ? (
+      <div className="row row-cols-1 row-cols-md-4 g-3">
+        {hospitals.map((hospital) => (
+          <div className="col" key={hospital.hospitalId.toString()}>
+            <Card
+              address={hospital.address}
+              availability={getAvailability(hospital)}
+              buttonText={buttonText}
+              department={hospital.department}
+              hospitalId={hospital.hospitalId}
+              locale={locale}
+              location={hospital.location}
+              name={hospital.name}
+              phone={hospital.phone}
+              website={hospital.website}
+            />
+          </div>
+        ))}
+      </div>
+    ) : (
+      <div style={{ textAlign: 'center' }}>
+        <p className="lead"><i>No hospitals.</i></p>
+      </div>
+    ));
   return (
     <div>
-      <div style={{marginTop: 20}}>
+      <div style={{ marginTop: 20 }}>
         <h3>{strings.hospitalsWithAppointmentsTitle[locale]}</h3>
         <p><i>{strings.hospitalsWithAppointmentsSubtitle[locale]}</i></p>
         {makeCardGrid(availableHospitals, strings.buttons.getAppointment[locale])}
       </div>
-      <div style={{marginTop: 20}}>
+      <div style={{ marginTop: 20 }}>
         <h3>{strings.hospitalsWithNoDataTitle[locale]}</h3>
         <p><i>{strings.hospitalsWithNoDataSubtitle[locale]}</i></p>
         {makeCardGrid(noDataHospitals, strings.buttons.visitWebsite[locale])}
       </div>
-      <div style={{marginTop: 20}}>
+      <div style={{ marginTop: 20 }}>
         <h3>{strings.hospitalsWithNoAppointmentsTitle[locale]}</h3>
         <p><i>{strings.hospitalsWithNoAppointmentsSubtitle[locale]}</i></p>
         {makeCardGrid(unavailableHospitals, strings.buttons.visitWebsite[locale])}
