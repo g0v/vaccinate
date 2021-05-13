55374508fa5e8e10d21f997b40ab618ba006d659
Kevin Liao <me@liaokev.in>
2021-05-14 06:43:41 +0800

    Fix Pyre errors.

--- backend/tests/integration/test_changgung_chiayi.py
+++ backend/tests/integration/test_changgung_chiayi.py
@@ -6,7 +6,7 @@ from hospital_types import AppointmentAvailability, HospitalAvailabilitySchema
 
 
 class TestChanggungChiayi(unittest.TestCase):
-    def test_full(self):
+    def test_full(self) -> None:
         with open("backend/tests/saved_pages/changgung_chiayi_full.html") as html_file:
             expected_availability: HospitalAvailabilitySchema = {
                 "self_paid": AppointmentAvailability.UNAVAILABLE,
@@ -15,7 +15,7 @@ class TestChanggungChiayi(unittest.TestCase):
             availability = parse_changgung_chiayi(html_file.read())
             self.assertEqual(availability, (21, expected_availability))
 
-    def test_available(self):
+    def test_available(self) -> None:
         with open(
             "backend/tests/saved_pages/changgung_chiayi_available.html"
         ) as html_file:
