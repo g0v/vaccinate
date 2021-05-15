c7b21940cbe9ae62839d46069048a442a64b7eed
Kevin Liao <me@liaokev.in>
2021-05-15 15:01:11 +0800

    Rename to Bahasa Indonesia.

--- frontend/Components/Content.jsx
+++ frontend/Components/Content.jsx
@@ -133,7 +133,7 @@ export default function Content(props: {
         >
           <button type="button" onClick={() => { setLanguage('enUS'); setLocale('en'); }} className="btn btn-outline-primary">English</button>
           <button type="button" onClick={() => { setLanguage('zhTW'); setLocale('zh'); }} className="btn btn-outline-primary">華語</button>
-          <button type="button" onClick={() => { setLanguage('id'); setLocale('en'); }} className="btn btn-outline-primary">Bahasa</button>
+          <button type="button" onClick={() => { setLanguage('id'); setLocale('en'); }} className="btn btn-outline-primary">Bahasa Indonesia</button>
           <button type="button" onClick={() => { setLanguage('ja'); setLocale('en'); }} className="btn btn-outline-primary">日本語</button>
           <button type="button" onClick={() => { setLanguage('ph'); setLocale('en'); }} className="btn btn-outline-primary">Tagalog</button>
         </div>
