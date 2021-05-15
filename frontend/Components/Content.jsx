fd4c5e6c3afa07bb96268c827b1b5a7d9f6e59a2
Marge <marge.consunji@gmail.com>
2021-05-15 23:48:51 +0800

    Merged conflicts in Content and added Tagalog translations for self-paid vaccine closer strings

--- frontend/Components/Content.jsx
+++ frontend/Components/Content.jsx
@@ -116,7 +116,7 @@ Maari kang makatanggap ng libreng bakuna laban sa COVID-19 kung ikaw ay nabibila
 Sa ngayon, ang gobyerno ng Taiwan ay gumagamit ng bakunang AstraZeneca.
 
 ### Paano gamitin ang website na ito?
-Hanapin ang mga ospital sa inyong lugar. Kung may nakalahad na "Available" sa itaas ng pangalan ng ospital, maari kayong magpaiskedyul ng appointment para sa bakuna na sarili mong babayaran. Kung "Unavailable", sa ngayon, wala pa pong makukuhang bakuna sa ospital na iyon. Kung "No Data" ang nakalahad, wala pa kaming mahahanap na impormasyon para sa ospital na iyon. Sa ganitong kaso, i-click ang "Puntahan ang website ng Ospital" upang maicheck at makipagiskedyul ng appointment sa ospital na iyon.
+Hanapin ang mga ospital sa inyong lugar. Kung may nakalahad na "Available" sa itaas ng pangalan ng ospital, maari kayong magpaiskedyul ng appointment para sa bakuna na sarili mong babayaran. Kung "Unavailable", sa ngayon, wala pa pong makukuhang bakuna sa ospital na iyon. Kung "No Data" ang nakalahad, wala pa kaming mahahanap na impormasyon para sa ospital na iyon. Sa ganitong kaso, i-click ang "Bisitahin ang Website" upang maicheck at makipagiskedyul ng appointment sa ospital na iyon.
 
 ### Saan ako pwedeng makakuha ng karagdagang impormasyon tungkol sa bakuna laban sa COVID-19? 
 * [FAQs sa Ingles galing sa Taiwan CDC](https://www.cdc.gov.tw/File/Get/rJJ09nktKU7btX_ZTEo_4w)
@@ -125,8 +125,12 @@ Hanapin ang mga ospital sa inyong lugar. Kung may nakalahad na "Available" sa it
 Itong website ay gawa ng mga boluntaryo ng [g0v](https://g0v.tw) civic hacker network. Hindi ito konektado sa gobyerno ng Taiwan. Hindi rin ito isang opisyal na pahayag ng gobyerno. Para sa opisyal na impormasyon tungkol dito, maari kayong pumunta sa [website ng Taiwan CDC](https://cdc.gov.tw).
 
 ### Paano ako makakatulong?
+<<<<<<< HEAD
 Ang impormasyon na nakikita mo sa website na ito ay galing sa mga web crawlers na naghahanap ng real-time na impormasyon galing sa mga website ng mga ospital sa Taiwan. Kailangan namin ang tulong ninyo para mapalago ang impormasyon na ito. Kung interesado kayo, pwede kayong sumali sa aming slack channel o i-fork ang aming code sa GitHub.
 >>>>>>> Added Tagalog translation and changed setLocale() method call
+=======
+Ang impormasyon na nakikita mo sa website na ito ay galing sa mga web crawlers na naghahanap ng real-time na impormasyon galing sa mga website ng mga ospital sa Taiwan. Kailangan namin ang tulong ninyo para mapalago ang impormasyon na ito. Kung interesado kayo, pwede kayong sumali sa aming slack channel o i-fork ang aming code sa [GitHub](https://github.com/g0v/vaccinate).
+>>>>>>> Changed 'Visit Hospital Website' translation
 `;
 
 function getContent(language: string): string {
