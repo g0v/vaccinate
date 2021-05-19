43af2d4a53a65aea52b7fa559731a18fd56ea5d6
Kevin Liao <me@liaokev.in>
2021-05-19 15:50:10 +0800

    CSV and JSON parsing for migration script (if necessary.)

---
+++ backend/migrate.py
@@ -0,0 +1,36 @@
+from hospital_types import (
+    Hospital,
+    AppointmentAvailability,
+)
+import csv,json
+
+# Code for combining data from the `hospitals.csv` with the
+# `hospitals.json` file that was taken from the MOHW website.
+
+def main():
+    csvfile = open("../data/hospitals.csv")
+    reader = csv.DictReader(csvfile)
+    rows = []
+    for row in reader:
+        hospital_id = int(row["編號"])
+        hospital: Hospital = {
+            "address": row["地址"],
+            "selfPaidAvailability": AppointmentAvailability.NO_DATA,
+            "department": row["科別"],
+            "governmentPaidAvailability": AppointmentAvailability.NO_DATA,
+            "hospitalId": int(row["編號"]),
+            "location": row["縣市"],
+            "name": row["醫院名稱"],
+            "phone": row["電話"],
+            "website": row["Website"],
+        }
+        rows.append(hospital)
+    jsonfile = open("../data/hospitals.json")
+    json_blob = json.loads(jsonfile.read())
+    print(json_blob)
+
+main()    
+
+if __name__ == 'main':
+    main()
+
