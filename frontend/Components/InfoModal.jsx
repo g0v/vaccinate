3368531d54eb569e73d66ac1f538d13eafaa6276
Sean Gau <rrtw0627@gmail.com>
2021-07-13 19:48:33 +0800

    get popup news from airtable

--- frontend/Components/InfoModal.jsx
+++ frontend/Components/InfoModal.jsx
@@ -3,6 +3,13 @@
 import * as React from 'react';
 
 export default function InfoModal(): React.Node {
+  const [news, setRows] = React.useState({ text: '獲取資料中...', buttons: [] });
+  const apiURL = process.env.API_URL || '';
+  React.useEffect(() => {
+    const url = '/popup_news';
+    fetch(apiURL + url).then((data) => data.json()).then((res) => setRows(res));
+  }, []); // Empty list makes this useEffect similar to componentDidMount();
+
   return (
     <div className="modal fade" id="InfoModal" tabIndex="-1" aria-labelledby="InfoModalLabel" aria-hidden="true">
       <div className="modal-dialog">
@@ -12,9 +19,15 @@ export default function InfoModal(): React.Node {
           </div>
           <div className="modal-body text-center p-4">
             <h5>這是個整理1922以外的預約方式的網站</h5>
-            <p>所有第9類—高風險疾病者、罕見疾病及重大傷病患，或第6、8、10類—民國60年(含)以前出生者，需經由 1922網站進行預約。</p>
+            <p>{news.text}</p>
             <div className="d-grid">
-              <a role="button" className="btn btn-outline-primary mb-2" href="https://1922.gov.tw/vas/" target="_blank" rel="noreferrer">前往 1922 網站</a>
+              {
+                news.buttons.map(
+                  (button) => (
+                    <a role="button" className="btn btn-outline-primary mb-2" href={button.words} target="_blank" rel="noreferrer">{button.title}</a>
+                  ),
+                )
+              }
               <button type="button" className="btn btn-primary mb-2" data-bs-dismiss="modal" aria-label="Close">進入本網站 開始找疫苗</button>
             </div>
           </div>
